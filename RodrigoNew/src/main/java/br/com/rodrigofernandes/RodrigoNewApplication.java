ed7c6e2c693b072b6a0c70c710e93f6cc7e0e528
Rodrigo Fernandes <rodrigofernandes3@hotmail.com>
2023-01-17 14:47:54 -0300

    Atualizações

--- RodrigoNew/src/main/java/br/com/rodrigofernandes/RodrigoNewApplication.java
+++ RodrigoNew/src/main/java/br/com/rodrigofernandes/RodrigoNewApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RodrigoNewApplication {
 
+	//TESTE TOKEN
 	public static void main(String[] args) {
 		SpringApplication.run(RodrigoNewApplication.class, args);
 	}
