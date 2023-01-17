c54f5fdac54620562a175b03a03fb1518402511b
Rodrigo Fernandes <rodrigofernandes3@hotmail.com>
2023-01-17 15:12:02 -0300

    Sincronizando conteúdos

---
+++ src/main/java/br/com/rodrigofernandes/RodrigoNewApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigofernandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodrigoNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodrigoNewApplication.class, args);
+	}
+
+}
